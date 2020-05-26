7e0a8567611217bd2697c2e1a5fa8e8d65b44bd8
Arash Rezaee <rezay.arash@gmail.com>
2020-05-26 13:40:02 +0430

    test

---
+++ vakavic/App_Start/WebApiConfig.Register.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // New code
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.Ena‌bleCors(cors);
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ vakavic/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http.Cors;
+
+namespace vakavic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlY2NhYTEyNWJlMWU1MzIzN2IzNDU4NSIsInZlcnNpb24iOjEsImlhdCI6MTU5MDQ3MTE4Nn0.kIBMtqi7JMXYjUxwwqf_3gR65ruRxQfYBRYcssNmhpk";
+            return View((object)token);
+        }
+    }
+}
